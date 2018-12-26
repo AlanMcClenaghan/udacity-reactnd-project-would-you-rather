4982464a12a88c35eee2268e346564f453eea298
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2018-12-26 22:31:52 +0000

    Unanswered/Answered questions selection buttons

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,17 +4,72 @@ import QuestionList from './QuestionList'
 
 class Dashboard extends Component {
 
+  state = {
+    answeredQuestionsSelected: false
+  }
+
+  toggleUnanswered = (e) => {
+
+    this.setState(() => ({
+      answeredQuestionsSelected: false
+    }))
+
+  }
+
+  toggleAnswered = (e) => {
+    this.setState(() => ({
+      answeredQuestionsSelected: true
+    }))
+  }
+
   render() {
+
+    const { answeredQuestionsSelected } = this.state
+    const { authedUser, questions } = this.props
+
+    const questionsArray = Object.keys(questions).map((key) => questions[key])
+
+    let authedUserQuestionsArray = []
+
+    if (answeredQuestionsSelected) {
+      authedUserQuestionsArray = questionsArray.filter((question) => {
+        if (question.optionOne.votes.includes(authedUser) || question.optionTwo.votes.includes(authedUser)) {
+          console.log(question.id)
+          authedUserQuestionsArray.push(question.id)
+          console.log(authedUserQuestionsArray)
+          return question
+        }
+      })
+    } else {
+      authedUserQuestionsArray = questionsArray.filter((question) => {
+        if (!question.optionOne.votes.includes(authedUser) && !question.optionTwo.votes.includes(authedUser)) {
+          console.log(question.id)
+          authedUserQuestionsArray.push(question.id)
+          console.log(authedUserQuestionsArray)
+          return question
+        }
+      })
+    }
+
+    const authedUserQuestionsIds = authedUserQuestionsArray.map((question) => question["id"])
+
+    console.log(authedUserQuestionsIds)
+
     return (
       <div>
-        {/* Change to Answered/Unanswered buttons*/}
         <div className="selector">
-          <button className="selector-button"><h3>Unanswered Questions</h3></button>
-          <button className="selector-button"><h3>Answered Questions</h3></button>
+          <button
+            className={!answeredQuestionsSelected ? "selector-button selected" : "selector-button"}
+            onClick={this.toggleUnanswered}
+          ><h3>Unanswered Questions</h3></button>
+          <button
+            className={answeredQuestionsSelected ? "selector-button selected" : "selector-button"}
+            onClick={this.toggleAnswered}
+          ><h3>Answered Questions</h3></button>
         </div>
 
         <ul className="dashboard-list">
-          {this.props.questionsIds.map((id) => (
+          {authedUserQuestionsIds.map((id) => (
             <li key={id}>
               <QuestionList id={id} />
             </li>
@@ -26,10 +81,12 @@ class Dashboard extends Component {
   }
 }
 
-const mapStateToProps = ({ questions }) => {
+const mapStateToProps = ({ authedUser, questions }) => {
   return {
-    questionsIds: Object.keys(questions)
-      .sort((a, b) => questions[b].timestamp - questions[b].timestamp)
+    authedUser,
+    questions,
+    // questionsIds: Object.keys(questions)
+    //   .sort((a, b) => questions[b].timestamp - questions[b].timestamp)
   }
 }
 
